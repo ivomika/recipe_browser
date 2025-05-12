4e437d09c50bfb28e47bb3a2eac2e1e3680a30bb
ivomika <67991025+ivomika@users.noreply.github.com>
2025-05-12 23:12:08 +0300

    исправил ошибки not_foun_page

--- lib/pages/not_found_page/view/not_found_page.dart
+++ lib/pages/not_found_page/view/not_found_page.dart
@@ -5,27 +5,29 @@ class NotFoundPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return LayoutBuilder(
-        builder: (context, constraints){
-            if(constraints.maxWidth <= 480){
-              return Image.asset(
-                  'assets/not_found_page/not_found_mobile.png',
+    return Scaffold(
+      body: LayoutBuilder(
+          builder: (context, constraints){
+              if(constraints.maxWidth <= 480){
+                return Image.asset(
+                    'assets/not_found/not_found_mobile.png',
+                    fit: BoxFit.cover,
+                );
+              }
+      
+              if(constraints.maxWidth <= 768){
+                return Image.asset(
+                  'assets/not_found/not_found_tablet.png',
                   fit: BoxFit.cover,
-              );
-            }
-
-            if(constraints.maxWidth <= 768){
+                );
+              }
+      
               return Image.asset(
-                'assets/not_found_page/not_found_tablet.png',
+                'assets/not_found/not_found_desktop.png',
                 fit: BoxFit.cover,
               );
-            }
-
-            return Image.asset(
-              'assets/not_found_page/not_found_desktop.png',
-              fit: BoxFit.cover,
-            );
-        }
+          }
+      ),
     );
   }
 }
