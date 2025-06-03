c75009a97aff49df96754cb87c33bc3bf4bb90f3
ivomika <67991025+ivomika@users.noreply.github.com>
2025-06-03 18:03:21 +0300

    разделил страницы

--- lib/pages/pages.dart
+++ lib/pages/pages.dart
@@ -1,6 +1,6 @@
-//GENERATED BARREL FILE
-export './detail_page/view/recipe_cooking_page.dart'; 
-export './detail_page/view/recipe_detail_page.dart'; 
+//GENERATED BARREL FILE 
 export './home_page/view/home_page.dart'; 
 export './not_found_page/view/not_found_page.dart'; 
+export './recipe_cooking_page/view/recipe_cooking_page.dart'; 
+export './recipe_detail_page/view/recipe_detail_page.dart';
 export './create_page/create_page.dart';
