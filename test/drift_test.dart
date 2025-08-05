842277635ca1ddadee8f3daf3f28f6e104188f8c
Akimov Ilya <akimoviv@sushivesla.team>
2025-08-05 23:14:57 +0300

    написал тесты для бд

---
+++ test/drift_test.dart
@@ -0,0 +1,294 @@
+import 'package:drift/drift.dart';
+import 'package:drift/native.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:recipe_browser/app/storage/drift/database/drift_app_database.dart';
+import 'package:recipe_browser/app/storage/drift/tabels/count_type/repository/drift_count_type_repository.dart';
+import 'package:recipe_browser/app/storage/drift/tabels/recipe/repository/drift_recipe_repository.dart';
+import 'package:recipe_browser/entities/entities.dart';
+
+void main() {
+  late DriftAppDatabase database = DriftAppDatabase(NativeDatabase.memory());
+  late IRecipeRepository recipeRepository = DriftRecipeRepository(database);
+  late ICountTypeRepository countTypeRepository = DriftCountTypeRepository(database);
+
+  tearDownAll(() async {
+    await database.close();
+  });
+
+  group(
+      'test count type CRUD',
+      (){
+        late CountType countType;
+
+        test(
+            'create',
+            () async {
+              final model = CountType.create(
+                  name: 'Kg'
+              );
+
+              countType = await countTypeRepository.create(model);
+
+              expect(
+                  countType.name,
+                  model.name,
+              );
+            }
+        );
+
+        test(
+            'update',
+                () async {
+              final newModel = CountType(
+                  id: countType.id,
+                  createdAt: countType.createdAt,
+                  name: 'Mlg'
+              );
+
+              countType = await countTypeRepository.update(
+                  newModel
+              );
+
+              expect(
+                  countType,
+                  newModel,
+              );
+            }
+        );
+
+        test(
+            'all',
+                () async {
+                await countTypeRepository.create(CountType.create(name: 'kg'));
+                await countTypeRepository.create(CountType.create(name: 'lit'));
+                await countTypeRepository.create(CountType.create(name: 'gram'));
+
+                final result = await countTypeRepository.all();
+
+                expect(
+                    result.length,
+                    (await database.countTypeTable.all().get()).length,
+                );
+            }
+        );
+
+        test(
+            'by id',
+            () async {
+                final result = await countTypeRepository.byId(countType.id);
+
+                expect(
+                    result,
+                    countType
+                );
+            }
+        );
+
+        test(
+            'by ids',
+            () async {
+                final all = await countTypeRepository.all();
+
+                final result = await countTypeRepository.byIds([countType.id, all.last.id]);
+
+                expect(
+                    result.length,
+                    2
+                );
+                expect(
+                    result.first.id,
+                    countType.id
+                );
+                expect(
+                    result.last.id,
+                    all.last.id
+                );
+            }
+        );
+
+        test(
+            'delete',
+            () async {
+                final oldCount = (await countTypeRepository.all()).length;
+                final result = await countTypeRepository.delete(countType);
+                final newCount = (await countTypeRepository.all()).length;
+
+                expect(
+                    result,
+                    true
+                );
+                expect(
+                    newCount,
+                    oldCount - 1
+                );
+
+            }
+        );
+      }
+  );
+
+  group(
+      'test recipe CRUD',
+          (){
+        late Recipe recipe;
+        late CountType countType;
+
+        test(
+            'create',
+                () async {
+              countType = await countTypeRepository.create(
+                  CountType.create(name: 'kg')
+              );
+
+              final model = Recipe.create(
+                  title: 'recipe',
+                  description: 'description',
+                  cookingTime: 10,
+                  kilocalories: 200,
+                  servings: 2,
+                  difficulty: 'hard',
+                  ingredients: [
+                    Ingredient(
+                        name: 'ingredient 1',
+                        count: 1,
+                        type: countType
+                    ),
+                    Ingredient(
+                        name: 'ingredient 2',
+                        count: 2,
+                        type: countType
+                    ),
+                  ],
+                  cookingSteps: [
+                    CookingStep(
+                        description: 'step 1'
+                    ),
+                    CookingStep(
+                        description: 'step 2'
+                    ),
+                    CookingStep(
+                        description: 'step 3'
+                    ),
+                  ]
+              );
+
+              recipe = await recipeRepository.create(model);
+
+              expect(
+                recipe.title,
+                model.title,
+              );
+              expect(
+                recipe.ingredients.length,
+                model.ingredients.length,
+              );
+              expect(
+                recipe.cookingSteps.length,
+                model.cookingSteps.length,
+              );
+            }
+        );
+
+        test(
+            'update',
+                () async {
+              final newModel = Recipe(
+                  id: recipe.id,
+                  createdAt: recipe.createdAt,
+                  title: 'recipe.title',
+                  description: recipe.description,
+                  cookingTime: recipe.cookingTime,
+                  kilocalories: recipe.kilocalories,
+                  servings: recipe.servings,
+                  difficulty: recipe.difficulty,
+                  ingredients: [
+                    ...recipe.ingredients,
+                    Ingredient(
+                        name: 'ingredient 3',
+                        count: 3,
+                        type: countType
+                    )
+                  ],
+                  cookingSteps: recipe.cookingSteps
+              );
+
+              recipe = await recipeRepository.update(
+                  newModel
+              );
+              
+              expect(
+                recipe,
+                newModel,
+              );
+              expect(
+                recipe.ingredients.length,
+                newModel.ingredients.length,
+              );
+              expect(
+                recipe.title,
+                newModel.title,
+              );
+            }
+        );
+
+        test(
+            'all',
+                () async {
+              final result = await recipeRepository.all();
+
+              expect(
+                result.length,
+                (await database.recipeTable.all().get()).length,
+              );
+            }
+        );
+
+        test(
+            'by id',
+                () async {
+              final result = await recipeRepository.byId(recipe.id);
+
+              expect(
+                  result,
+                  recipe
+              );
+            }
+        );
+
+        test(
+            'by ids',
+                () async {
+              final result = await recipeRepository.byIds([recipe.id]);
+
+              expect(
+                  result.length,
+                  1
+              );
+              expect(
+                  result.first.id,
+                  recipe.id
+              );
+            }
+        );
+
+        test(
+            'delete',
+                () async {
+              final oldCount = (await recipeRepository.all()).length;
+              final result = await recipeRepository.delete(recipe);
+              final newCount = (await recipeRepository.all()).length;
+
+              expect(
+                  result,
+                  true
+              );
+              expect(
+                  newCount,
+                  oldCount - 1
+              );
+
+            }
+        );
+      }
+  );
+}
